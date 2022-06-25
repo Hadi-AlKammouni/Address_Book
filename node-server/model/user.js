88a9b8cfa393a7a210673641bd358dcd4db44d1e
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-25 23:42:44 +0300

    BE//Adding relation between users & contacts collection

--- node-server/model/user.js
+++ node-server/model/user.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: { type: String },
   token: { type: String },
+  contacts:[{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'contact'
+  },],
 });
 
 module.exports = mongoose.model("user", userSchema);
