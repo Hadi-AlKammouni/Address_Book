1f49343a7c86866435b75af22b962eab64a8b703
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-25 23:41:08 +0300

    BE//Editing location in contact model

--- node-server/model/contact.js
+++ node-server/model/contact.js
@@ -5,8 +5,19 @@ const contactSchema = new mongoose.Schema({
   phone_number: {type: String, required: true},
   relationship_status: {type: String, required: true},
   email: {type: String, required: true},
-  location: {type: String, required: true},
-  user: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
+  // location: {type: String, required: true},
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: false,
+    },
+    coordinates: {
+      type: [Number],
+      required: false,
+    },
+  },
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 });
 
 module.exports = mongoose.model('contact', contactSchema);
