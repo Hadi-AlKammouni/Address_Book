f4d837d1741e06aa3d95f303f043de1a924d6567
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-25 23:47:16 +0300

    BE//Implementing the relation upon adding contact

--- node-server/src/contact/controller/contact.js
+++ node-server/src/contact/controller/contact.js
@@ -1,11 +1,24 @@
 const { addContact, getContacts } = require('../service');
 const Contact = require("../../../model/contact");
+const User = require("../../../model/user");
 
 async function add(req, res) {
   try {
   
     const newContact = await addContact(req.body);
     // console.log('newContact =>', newContact);
+
+    // use updateOne() 
+    const updateUser = await User.updateOne(
+      {
+        _id: newContact.user
+      },
+      {
+        $push: {
+          contacts: newContact._id
+        }
+      }
+    );
 
     return res.status(200).send(newContact);
   } 
