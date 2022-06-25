c8f41fe1d82083feac897fa9aaeb2630bb7d0fff
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-25 23:54:29 +0300

    BE//Testing the authenticating api

--- node-server/src/user/index.js
+++ node-server/src/user/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 // const { register, login, welcome } = require('./controller/user');
-const { get, register, login } = require('./controller/user');
+const { get, register, login, welcome } = require('./controller/user');
 
 const router = Router();
 // const verifyToken = require('../../middleware/auth');
+const userMiddleware = require("../../middleware/auth");
+router.get("/", userMiddleware(), (req, res) => welcome(req, res));
 
 router.post('/register', register);
 router.post('/login', login);
