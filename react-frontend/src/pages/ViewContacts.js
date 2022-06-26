45f1b9a58dbd7c065a6b1052b75fecace1cc66a8
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-26 16:05:52 +0300

    FE//Implementing getContacts functionality

--- react-frontend/src/pages/ViewContacts.js
+++ react-frontend/src/pages/ViewContacts.js
@@ -1,7 +1,48 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ViewContacts = () => {
-    return <div>ViewContacts here</div>;
+    const navigate = useNavigate();
+    const [contacts, setContacts] = useState([]);
+
+    const getContacts = async () => {
+        const res = await fetch("http://localhost:4001/api/contact/get_contact");
+        const data = await res.json();
+        return data;
+    };
+
+    //Get contacts
+    useEffect(() => {
+        const getData = async () => {
+          const contactsFromServer = await getContacts();
+          setContacts(contactsFromServer)
+        };
+        getData();
+    }, []);
+
+    return (
+    <div className="container">
+      {contacts.map((contact) => {
+        // console.log(contact)
+        return (
+          <div
+            key={contact._id}
+            className="contact"
+            onClick={() => {
+              navigate(`${contact._id}`);
+            }}
+          >
+            {/* Diplaying the contacts details */}
+            <h1>Contact name: {contact.name}</h1>
+            <h3>Phone number: {contact.phone_number}</h3>
+            <h3>Relatioship status: {contact.relationship_status}</h3>
+            <h3>Email: {contact.email}</h3>
+            <h3>Location: {contact.location.coordinates[0]} / {contact.location.coordinates[1]}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default ViewContacts;
