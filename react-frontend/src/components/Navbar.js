75a76b0a9485c15098d0cc85d855afd46275eb96
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-26 11:04:48 +0300

    FE//Displaying navbar items

--- react-frontend/src/components/Navbar.js
+++ react-frontend/src/components/Navbar.js
@@ -8,14 +8,29 @@ const Navbar = () => {
   const navItems = [
     { link: "register", name: "register" },
     { link: "login", name: "login" },
-    { link: "view_contact/:id", name: "view_contact" },
+    // { link: "view_contact/:id", name: "view_contact" },
   ];
 
     // //Catch the current path
     // console.log(location);
 
   return (
-    <div>Navbar</div>
+    <nav className="top-bar">
+      <ul className="nav-items">
+        {navItems.map((navItem, index) => {
+          return (
+            <NavLink key={index} to={navItem.link}>
+              <li>{navItem.name}</li>
+            </NavLink>
+          );
+        })}
+        {/* {location.pathname.includes("") && (
+          <NavLink to="view_contact/:id">
+            <li>View contact</li>
+          </NavLink>
+        )} */}
+      </ul>
+    </nav>
   );
 };
 
